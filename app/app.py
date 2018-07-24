ad5698259594502e7fecb9151eebc18527f0ec3c
yandi <yanfigg@gmail.com>
2018-07-24 18:14:15 +0800

    initial     student CRUD

---
+++ app/app.py
@@ -0,0 +1,235 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import abort
+from flask_cors import CORS
+from datetime import datetime
+
+from sqlalchemy import Column, String, Integer, create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer(), primary_key=True)
+    name = Column(String(64))
+    email = Column(String(64))
+    password = Column(String(16))
+
+    def info(self):
+        return dict(id=self.id,
+                    name=self.name,
+                    email=self.email,
+                    )
+
+
+class Student(Base):
+    __tablename__ = 'student'
+    id = Column(Integer(), primary_key=True, nullable=False)
+    student_name = Column(String(64), nullable=False)
+    gender = Column(Integer())
+    id_card_num = Column(Integer(), nullable=False)
+    phone_num = Column(Integer(), nullable=False)
+    job_title = Column(String(64))
+    enter_time = Column(Integer())
+    class_id = Column(Integer())
+    class_name = Column(String(64))
+    result = Column(Integer())
+
+    def info(self):
+        return dict(id=self.id,
+                    student_name=self.student_name,
+                    gender=self.gender,
+                    id_card_num=self.id_card_num,
+                    phone_num=self.phone_num,
+                    job_title=self.job_title,
+                    enter_time=self.enter_time,
+                    class_id=self.class_id,
+                    class_name=self.class_name,
+                    result=self.result
+                    )
+
+
+class Class(Base):
+    __tablename__ = 'class'
+    id = Column(Integer(), primary_key=True, nullable=False)
+    class_name = Column(String(64), nullable=False)
+    begin_address = Column(String(128))
+    begin_time = Column(Integer())
+    end_time = Column(Integer())
+    create_time = Column(Integer())
+
+    def info(self):
+        return dict(id=self.id,
+                    class_name=self.class_name,
+                    begin_address=self.begin_address,
+                    begin_time=self.begin_time,
+                    end_time=self.end_time,
+                    create_time=self.create_time,
+                    )
+
+
+class Test(Base):
+    __tablename__ = 'test'
+    id = Column(Integer(), primary_key=True, nullable=False)
+    test_name = Column(String(64), nullable=False)
+    test_type = Column(String(20))
+    test_work_type = Column(String(20))
+    target_name = Column(String(128))
+    target_id = Column(Integer())
+    start_time = Column(Integer())
+    end_time = Column(Integer())
+    duration = Column(Integer())
+    test_times = Column(Integer())
+    test_status = Column(String(10))
+
+    def info(self):
+        return dict(id=self.id,
+                    test_name=self.test_name,
+                    test_type=self.test_type,
+                    test_work_type=self.test_work_type,
+                    target_name=self.target_name,
+                    target_id=self.target_id,
+                    start_time=self.start_time,
+                    end_time=self.end_time,
+                    duration=self.duration,
+                    test_times=self.test_times,
+                    test_status=self.test_status
+                    )
+
+
+app = Flask(__name__)
+CORS(app,supports_credentials = True)
+
+@app.route('/students/', methods=['GET'])
+def get_students():
+    current_page = 1
+    page_size = 10
+    total = 0
+    if request.args:
+        if request.args['current_page'] and int(request.args['current_page']) > 0:
+            current_page = int(request.args['current_page'])
+        if request.args['page_size'] and int(request.args['page_size']) > 0:
+            page_size = int(request.args['page_size'])
+    try:
+        response = {
+            'list': [],
+            'code': 200,
+            'message':'succ',
+            'pagination':{
+                'current_page': current_page,
+                'page_size': page_size,
+                'total': total,
+            }
+        }
+        session = DBsession()
+        response['pagination']['total'] = session.query(Student).count()
+        ret = session.query(Student).order_by(Student.id.desc()).limit(
+            page_size).offset((current_page-1)*page_size)
+        for i in ret:
+            response['list'].append(i.info())
+        session.close()
+        return jsonify(response)
+    except:
+        return
+
+@app.route('/student/<int:id>', methods=['GET'])
+def get_student_by_id(id):
+    try:
+        response = {
+            'data': {},
+            'code': 200,
+            'message':'succ',
+        }
+        session = DBsession()
+        response['data'] = session.query(Student).filter_by(id=id).first().info()
+        session.close()
+        return jsonify(response)
+    except:
+        return
+
+@app.route('/add/student/', methods=['POST'])
+def add_student():
+    if not request.json:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        student = Student(
+            student_name = request.json['student_name'],
+            gender = request.json['gender'],
+            phone_num = request.json['phone_num'],
+            id_card_num = request.json['id_card_num'],
+            job_title = request.json['job_title'],
+            enter_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        )
+        session = DBsession()
+        session.add(student)
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message':'succ',
+        })
+    except:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+
+@app.route('/modify/student/<int:id>', methods=['PATCH'])
+def modify_student(id):
+    if not request.json or not id:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        session = DBsession()
+        for k,v in request.json.items():
+            if k and v :
+                session.query(Student).filter(Student.id == id).update({k:v})
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message':'succ',
+        })
+    except:
+        return jsonify({
+            'code': 400,
+            'message': 'inner error'
+        })
+
+@app.route('/delete/students/', methods=['DELETE'])
+def delete_students():
+    if not request.json:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        session = DBsession()
+        session.query(Student).filter(Student.id.in_(request.json['ids'])).delete(synchronize_session = False)
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message':'succ',
+        })
+    except:
+        return jsonify({
+            'code': 404,
+            'message': 'Not Found'
+        })
+
+
+if __name__ == '__main__':
+    engine = create_engine(
+    'mysql+mysqlconnector://root:password@localhost:3306/ams')
+    DBsession = sessionmaker(bind=engine)
+    app.run(host="192.168.1.58", port=5000, debug=True)
---
+++ app/schema.sql
@@ -0,0 +1,51 @@
+DROP TABLE if exists user;
+DROP TABLE if exists student;
+DROP TABLE if exists class;
+DROP TABLE if exists test;
+
+CREATE TABLE user (
+    id int(16) AUTO_INCREMENT NOT NULL,
+    name varchar(64) NOT NULL,
+    email varchar(64),
+    password varchar(16) NOT NULL,
+    primary key(id)
+);
+
+CREATE TABLE student (
+    id int(16) AUTO_INCREMENT NOT NULL,
+    student_name varchar(64) NOT NULL,
+    gender varchar(10),
+    id_card_num int(30) NOT NULL,
+    phone_num int(20),
+    job_title varchar(64),
+    enter_time int(20),
+    class_id int(10),
+    class_name varchar(64),
+    result int(3),
+    primary key(id)
+);
+
+CREATE TABLE class (
+    id int(16) AUTO_INCREMENT NOT NULL,
+    class_name varchar(64) NOT NULL,
+    begin_time int(20),
+    end_time int(20),
+    begin_address varchar(128),
+    create_time int(20),
+    primary key(id)
+);
+
+CREATE TABLE test (
+    id int(16) AUTO_INCREMENT NOT NULL,
+    test_name varchar(64) NOT NULL,
+    test_type varchar(20),
+    test_work_type varchar(20),
+    target_name varchar(128),
+    target_id varchar(128),
+    duration int(5),
+    start_time int(20),
+    end_time int(20),
+    test_times int(3),
+    test_status varchar(10),
+    primary key(id)
+);
