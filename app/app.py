d676fa37e58008d2d06a990dd319809af0633d0b
yandi <yanfigg@gmail.com>
2018-07-30 09:54:32 +0800

    classes服务修改

--- app/app.py
+++ app/app.py
@@ -3,7 +3,10 @@ from flask import request
 from flask import jsonify
 from flask import abort
 from flask_cors import CORS
+from sqlalchemy import and_, or_
 from datetime import datetime
+import logging
+logging.basicConfig(level=logging.INFO)
 
 from sqlalchemy import Column, String, Integer, create_engine
 from sqlalchemy.orm import sessionmaker
@@ -53,13 +56,14 @@ class Student(Base):
                     )
 
 
-class Class(Base):
+class Classes(Base):
     __tablename__ = 'class'
     id = Column(Integer(), primary_key=True, nullable=False)
     class_name = Column(String(64), nullable=False)
     begin_address = Column(String(128))
     begin_time = Column(Integer())
     end_time = Column(Integer())
+    course_plan = Column(String(128))
     create_time = Column(Integer())
 
     def info(self):
@@ -69,6 +73,7 @@ class Class(Base):
                     begin_time=self.begin_time,
                     end_time=self.end_time,
                     create_time=self.create_time,
+                    course_plan=self.course_plan,
                     )
 
 
@@ -102,10 +107,150 @@ class Test(Base):
 
 
 app = Flask(__name__)
-CORS(app,supports_credentials = True)
+CORS(app, supports_credentials=True)
+
 
 @app.route('/students/', methods=['GET'])
 def get_students():
+    current_page = 1
+    page_size = 10
+    total = 0
+    try:
+        response = {
+            'list': [],
+            'code': 200,
+            'message': 'succ',
+            'pagination': {
+                'current_page': current_page,
+                'page_size': page_size,
+                'total': total,
+            }
+        }
+        session = DBsession()
+        query = session.query(Student).filter(Student.id > 0)
+        if request.args:
+            if request.args.get('student_name',default = '') != '':
+                query = query.filter(or_(
+                    Student.student_name.like('%{e}%'.format(e=request.args['student_name'])),
+                    Student.id_card_num.like('%{e}%'.format(e=request.args['student_name'])),
+                    ))
+            if request.args.get('id_card_num',default = '') != '':
+                query = query.filter(Student.id_card_num == request.args['id_card_num'])
+            if request.args.get('current_page',default='') != '' and int(request.args['current_page']) > 0:
+                current_page = int(request.args['current_page'])
+            if request.args.get('page_size',default='') != '' and int(request.args['page_size']) > 0:
+                page_size = int(request.args['page_size'])
+        response['pagination']['total'] = query.count()
+        result = query.order_by(Student.id.desc()).limit(page_size).offset((current_page-1)*page_size).all()
+        for i in result:
+            response['list'].append(i.info())
+        session.close()
+        print('hehehehehehehe')
+        return jsonify(response)
+    except Exception as err:
+        logging.exception('error')
+        return 
+
+
+@app.route('/student/<int:id>', methods=['GET'])
+def get_student_by_id(id):
+    try:
+        response = {
+            'data': {},
+            'code': 200,
+            'message': 'succ',
+        }
+        session = DBsession()
+        response['data'] = session.query(
+            Student).filter_by(id=id).first().info()
+        session.close()
+        return jsonify(response)
+    except:
+        return
+
+
+@app.route('/add/student/', methods=['POST'])
+def add_student():
+    if not request.json:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        student = Student(
+            student_name=request.json['student_name'],
+            gender=request.json['gender'],
+            phone_num=request.json['phone_num'],
+            id_card_num=request.json['id_card_num'],
+            job_title=request.json['job_title'],
+            enter_time=datetime.now().strftime('%Y%m%d%H%M%S')
+        )
+        session = DBsession()
+        session.add(student)
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message': 'succ',
+        })
+    except:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+
+
+@app.route('/modify/student/<int:id>', methods=['PATCH'])
+def modify_student(id):
+    if not request.json or not id:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        session = DBsession()
+        for k, v in request.json.items():
+            if k and v:
+                session.query(Student).filter(Student.id == id).update({k: v})
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message': 'succ',
+        })
+    except:
+        return jsonify({
+            'code': 400,
+            'message': 'inner error'
+        })
+
+
+@app.route('/delete/students/', methods=['DELETE'])
+def delete_students():
+    if not request.json:
+        return jsonify({
+            'code': 400,
+            'message': 'param error'
+        })
+    try:
+        session = DBsession()
+        session.query(Student).filter(Student.id.in_(
+            request.json['ids'])).delete(synchronize_session=False)
+        session.commit()
+        session.close()
+        return jsonify({
+            'code': 200,
+            'message': 'succ',
+        })
+    except:
+        return jsonify({
+            'code': 404,
+            'message': 'Not Found'
+        })
+
+
+@app.route('/classes/', methods=['GET'])
+def get_classes():
     current_page = 1
     page_size = 10
     total = 0
@@ -118,16 +263,16 @@ def get_students():
         response = {
             'list': [],
             'code': 200,
-            'message':'succ',
-            'pagination':{
+            'message': 'succ',
+            'pagination': {
                 'current_page': current_page,
                 'page_size': page_size,
                 'total': total,
             }
         }
         session = DBsession()
-        response['pagination']['total'] = session.query(Student).count()
-        ret = session.query(Student).order_by(Student.id.desc()).limit(
+        response['pagination']['total'] = session.query(Classes).count()
+        ret = session.query(Classes).order_by(Classes.id.desc()).limit(
             page_size).offset((current_page-1)*page_size)
         for i in ret:
             response['list'].append(i.info())
@@ -136,53 +281,57 @@ def get_students():
     except:
         return
 
-@app.route('/student/<int:id>', methods=['GET'])
-def get_student_by_id(id):
+
+@app.route('/class/<int:id>', methods=['GET'])
+def get_class_by_id(id):
     try:
         response = {
             'data': {},
             'code': 200,
-            'message':'succ',
+            'message': 'succ',
         }
         session = DBsession()
-        response['data'] = session.query(Student).filter_by(id=id).first().info()
+        response['data'] = session.query(
+            Classes).filter_by(id=id).first().info()
         session.close()
         return jsonify(response)
     except:
         return
 
-@app.route('/add/student/', methods=['POST'])
-def add_student():
+
+@app.route('/add/class/', methods=['POST'])
+def add_class():
     if not request.json:
         return jsonify({
             'code': 400,
             'message': 'param error'
         })
     try:
-        student = Student(
-            student_name = request.json['student_name'],
-            gender = request.json['gender'],
-            phone_num = request.json['phone_num'],
-            id_card_num = request.json['id_card_num'],
-            job_title = request.json['job_title'],
-            enter_time = datetime.now().strftime('%Y%m%d%H%M%S')
+        cla = Classes(
+            class_name=request.json['class_name'],
+            begin_time=int(request.json['begin_time']),
+            end_time=int(request.json['end_time']),
+            begin_address=request.json['begin_address'],
+            course_plan=request.json['course_plan'],
+            create_time=int(datetime.now().strftime('%Y%m%d%H%M%S'))
         )
         session = DBsession()
-        session.add(student)
+        session.add(cla)
         session.commit()
         session.close()
         return jsonify({
             'code': 200,
-            'message':'succ',
+            'message': 'succ',
         })
     except:
         return jsonify({
             'code': 400,
-            'message': 'param error'
+            'message': 'server error'
         })
 
-@app.route('/modify/student/<int:id>', methods=['PATCH'])
-def modify_student(id):
+
+@app.route('/modify/class/<int:id>', methods=['PATCH'])
+def modify_class(id):
     if not request.json or not id:
         return jsonify({
             'code': 400,
@@ -190,14 +339,14 @@ def modify_student(id):
         })
     try:
         session = DBsession()
-        for k,v in request.json.items():
-            if k and v :
-                session.query(Student).filter(Student.id == id).update({k:v})
+        for k, v in request.json.items():
+            if k and v:
+                session.query(Classes).filter(Classes.id == id).update({k: v})
         session.commit()
         session.close()
         return jsonify({
             'code': 200,
-            'message':'succ',
+            'message': 'succ',
         })
     except:
         return jsonify({
@@ -205,8 +354,9 @@ def modify_student(id):
             'message': 'inner error'
         })
 
-@app.route('/delete/students/', methods=['DELETE'])
-def delete_students():
+
+@app.route('/delete/classes/', methods=['DELETE'])
+def delete_classes():
     if not request.json:
         return jsonify({
             'code': 400,
@@ -214,12 +364,13 @@ def delete_students():
         })
     try:
         session = DBsession()
-        session.query(Student).filter(Student.id.in_(request.json['ids'])).delete(synchronize_session = False)
+        session.query(Classes).filter(Classes.id.in_(
+            request.json['ids'])).delete(synchronize_session=False)
         session.commit()
         session.close()
         return jsonify({
             'code': 200,
-            'message':'succ',
+            'message': 'succ',
         })
     except:
         return jsonify({
@@ -230,6 +381,6 @@ def delete_students():
 
 if __name__ == '__main__':
     engine = create_engine(
-    'mysql+mysqlconnector://root:password@localhost:3306/ams')
+        'mysql+mysqlconnector://root:password@localhost:3306/ams')
     DBsession = sessionmaker(bind=engine)
     app.run(host="192.168.1.58", port=5000, debug=True)
